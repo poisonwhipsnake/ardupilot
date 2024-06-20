d296d5c6ae605165d66cb1d765ddaec5f5b1bb9e
Josh <josh@ornata.ag>
2024-06-20 17:49:58 +1000

    Fixed "Internal Error" Issue

--- ArduCopter/Copter.cpp
+++ ArduCopter/Copter.cpp
@@ -149,10 +149,11 @@ const AP_Scheduler::Task Copter::scheduler_tasks[] = {
 
     SCHED_TASK(rc_loop,              250,    130,  3),
     SCHED_TASK(throttle_loop,         50,     75,  6),
-    SCHED_TASK(update_wheel_encoder,   100,    200,  36),
+    
 #if AP_FENCE_ENABLED
     SCHED_TASK(fence_check,           25,    100,  7),
 #endif
+    SCHED_TASK(update_wheel_encoder,  100,    100,  8),
     SCHED_TASK_CLASS(AP_GPS,               &copter.gps,                 update,          50, 200,   9),
 #if AP_OPTICALFLOW_ENABLED
     SCHED_TASK_CLASS(AP_OpticalFlow,          &copter.optflow,             update,         200, 160,  12),
--- ArduCopter/sensors.cpp
+++ ArduCopter/sensors.cpp
@@ -176,6 +176,7 @@ bool Copter::get_rangefinder_height_interpolated_cm(int32_t& ret) const
 // update wheel encoders
 void Copter::update_wheel_encoder()
 {
+    
     // exit immediately if not enabled
     if (g2.wheel_encoder.num_sensors() == 0) {
         return;
@@ -189,57 +190,6 @@ void Copter::update_wheel_encoder()
         wheel_encoder_wheel_angle[i] = g2.wheel_encoder.get_wheel_angle(i);
         wheel_encoder_raw_angle[i] = g2.wheel_encoder.get_raw_angle(i);
     }
+    
 
-    // send wheel encoder delta angle and delta time to EKF
-    // this should not be done at more than 50hz
-    // initialise on first iteration
-    if (!wheel_encoder_initialised) {
-        wheel_encoder_initialised = true;
-        for (uint8_t i = 0; i < g2.wheel_encoder.num_sensors(); i++) {
-            wheel_encoder_last_angle_rad[i] = g2.wheel_encoder.get_delta_angle(i);
-            wheel_encoder_last_reading_ms[i] = g2.wheel_encoder.get_last_reading_ms(i);
-        }
-        return;
-    }
-
-    // on each iteration send data from alternative wheel encoders
-    wheel_encoder_last_index_sent++;
-    if (wheel_encoder_last_index_sent >= g2.wheel_encoder.num_sensors()) {
-        wheel_encoder_last_index_sent = 0;
-    }
-
-    // get current time, total delta angle (since startup) and update time from sensor
-    const float curr_angle_rad = g2.wheel_encoder.get_delta_angle(wheel_encoder_last_index_sent);
-    const uint32_t sensor_reading_ms = g2.wheel_encoder.get_last_reading_ms(wheel_encoder_last_index_sent);
-    const uint32_t now_ms = AP_HAL::millis();
-
-    // calculate angular change (in radians)
-#if HAL_NAVEKF3_AVAILABLE
-    const float delta_angle = curr_angle_rad - wheel_encoder_last_angle_rad[wheel_encoder_last_index_sent];
-#endif
-    wheel_encoder_last_angle_rad[wheel_encoder_last_index_sent] = curr_angle_rad;
-
-    // calculate delta time using time between sensor readings or time since last send to ekf (whichever is shorter)
-    uint32_t sensor_diff_ms = sensor_reading_ms - wheel_encoder_last_reading_ms[wheel_encoder_last_index_sent];
-    if (sensor_diff_ms == 0 || sensor_diff_ms > 100) {
-        // if no sensor update or time difference between sensor readings is too long use time since last send to ekf
-        sensor_diff_ms = now_ms - wheel_encoder_last_reading_ms[wheel_encoder_last_index_sent];
-        wheel_encoder_last_reading_ms[wheel_encoder_last_index_sent] = now_ms;
-    } else {
-        wheel_encoder_last_reading_ms[wheel_encoder_last_index_sent] = sensor_reading_ms;
-    }
-#if HAL_NAVEKF3_AVAILABLE
-    const float delta_time = sensor_diff_ms * 0.001f;
-
-    /* delAng is the measured change in angular position from the previous measurement where a positive rotation is produced by forward motion of the vehicle (rad)
-     * delTime is the time interval for the measurement of delAng (sec)
-     * timeStamp_ms is the time when the rotation was last measured (msec)
-     * posOffset is the XYZ body frame position of the wheel hub (m)
-     */
-    ahrs.EKF3.writeWheelOdom(delta_angle,
-                        delta_time,
-                        wheel_encoder_last_reading_ms[wheel_encoder_last_index_sent],
-                        g2.wheel_encoder.get_pos_offset(wheel_encoder_last_index_sent),
-                        g2.wheel_encoder.get_wheel_radius(wheel_encoder_last_index_sent));
-#endif
 }
