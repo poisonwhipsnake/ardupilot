e4e29f514d941d31423413157f1cfd7ee446eef7
Josh <johndoe@example.com>
2025-03-31 16:23:04 +1100

    Safety Manager Scripts

---
+++ libraries/AP_Scripting/Ornata/safety_manager_case.lua
@@ -0,0 +1,46 @@
+-- Safety Manager Script
+
+
+
+gpio:pinMode(58,1) -- set servo 10 to output, gpio:pinMode(51,0) would be input
+
+gpio:pinMode(59,1) -- set servo 10 to output, gpio:pinMode(51,0) would be input
+
+gpio:pinMode(57,0) -- set servo 9 to input, gpio:pinMode(51,0) would be input
+
+auth_id = arming:get_aux_auth_id()
+
+tractor_estop_armed = false
+
+function update()
+  
+  -- if input changes state, send_tex
+
+  local tractor_estop_status = gpio:read(57)
+
+  if tractor_estop_status and (not tractor_estop_armed) then
+    gcs:send_text(0, "Safety Manager: Tractor E-Stop Armed")
+  elseif (not tractor_estop_status) and tractor_estop_armed then
+    gcs:send_text(0, "Safety Manager: Tractor E-Stop Disarmed")
+  end
+
+  tractor_estop_armed = tractor_estop_status
+
+  if tractor_estop_armed then
+    arming:set_aux_auth_passed(auth_id)
+  else
+    arming:set_aux_auth_failed(auth_id, "Tractor ESTOP Not Armed")
+  end
+  
+  if arming:is_armed() then
+    gpio:write(58, 0)
+    gpio:write(59, 0)
+  else
+    gpio:write(58, 1)
+    gpio:write(59, 1)
+  end
+
+  return update, 1000
+end
+
+return update() -- run immediately before starting to reschedule
